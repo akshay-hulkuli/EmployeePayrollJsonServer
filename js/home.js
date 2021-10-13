60b2945528caa6e695d855527c3e92cd81062776
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-13 20:48:12 +0530

    REFACTORED: to display employee details from JSON object

--- js/home.js
+++ js/home.js
@@ -4,22 +4,54 @@ window.addEventListener('DOMContentLoaded',(event) =>{
 
 const createInnerHtml = () => {
     const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
+    let empPayrollData = createEmployeePayrollJSON()[1];
     const innerHtml = ` ${headerHtml}
     <tr>
-        <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png"></td>
-        <td>Narayan Mahadevan</td>
-        <td>Male</td>
+        <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
         <td>
-            <div class="dept-label">HR</div>
-            <div class="dept-label">Finance</div>
+            <div class="dept-label">${empPayrollData._department[0]}</div>
+            <div class="dept-label">${empPayrollData._department[1]}</div>
         </td>
-        <td>300000</td>
-        <td>24 APR 2020</td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
         <td>
-            <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-            <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+            <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+            <img name="${empPayrollData._id}"" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
         </td>
     </tr>
     `;
     document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Narayan Mahadeven',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary: '500000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -2.png'
+        },
+        {
+            _name: 'Amarpa Shashanka Keerthi Kumar',
+            _gender: 'female',
+            _department: [
+                'Sales'
+            ],
+            _salary: '400000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime()+1,
+            _profilePic: '../assets/profile-images/Ellipse -1.png'
+        }
+    ];
+    return empPayrollListLocal;
 }
